de94ee1b2927bd9df3e5f30bc78618bc4c6a5a16
유은철 <axmin123@naver.com>
2021-05-31 22:38:28 +0900

    productbuy 코드 일부수정
    private ProductbuyBackgroundVersion pbv; 삭제
    new BuyFrame("결제화면",600,800,this);
    >> new BuyFrame("결제화면",600,800); 변경

--- JavaProjectDB/src/MoGong/Productbuy.java
+++ JavaProjectDB/src/MoGong/Productbuy.java
@@ -32,7 +32,7 @@ private JLabel name,price,info,wait;
 private ImageIcon img,back,infoimg;
 private JScrollPane scrollpane;
 private JLabel jungga;
-private ProductbuyBackgroundVersion pbv;
+private Productbuy pbv;
 	
 	
 
@@ -156,7 +156,7 @@ private ProductbuyBackgroundVersion pbv;
 		Object obj = e.getSource();
 		
 		if(obj == but1) {
-			pbv = new ProductbuyBackgroundVersion("구매화면",650,800,this);
+			pbv = new Productbuy("구매화면",650,800);
 			pbv.setLocation(900, 150);
 		}
 		
