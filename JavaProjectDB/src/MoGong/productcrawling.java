2555362be13c2aee260b60703ebd03859331d048
유은철 <axmin123@naver.com>
2021-06-01 01:33:34 +0900

    상품 업데이트닷

--- JavaProjectDB/src/MoGong/productcrawling.java
+++ JavaProjectDB/src/MoGong/productcrawling.java
@@ -17,7 +17,9 @@ public class productcrawling {
 	
 	static int i = 0;
 			static void Crawling(ArrayList<productdto> pl) throws IOException {
-			
+		
+				
+				// 구찌 파트
 		crawlingproduct(pl,
 				"https://www.coupang.com/np/search?rocketAll=false&q=%EA%B5%AC%EC%B0%8C+%EC%8B%9C%EA%B3%84&brand=&offerCondition=&filter=&availableDeliveryFilter=&filterType=&isPriceRange=false&priceRange=&minPrice=&maxPrice=&page=1&trcid=&traid=&filterSetByUser=true&channel=user&backgroundColor=&component=&rating=0&sorter=salePriceDesc&listSize=36",
 				"구찌",
@@ -40,7 +42,77 @@ public class productcrawling {
 				);
 			
 		
+			// 샤넬 파트
+		crawlingproduct(pl,
+				"https://www.coupang.com/np/search?rocketAll=false&q=%EC%83%A4%EB%84%AC+%EC%8B%9C%EA%B3%84&brand=&offerCondition=&filter=&availableDeliveryFilter=&filterType=&isPriceRange=false&priceRange=&minPrice=&maxPrice=&page=1&trcid=&traid=&filterSetByUser=true&channel=user&backgroundColor=&component=&rating=0&sorter=salePriceDesc&listSize=36",
+				"샤넬",
+				"시계"
+				);
+		crawlingproduct(pl,
+				"https://www.coupang.com/np/search?rocketAll=false&q=%EC%83%A4%EB%84%AC+%EA%B0%80%EB%B0%A9&brand=&offerCondition=&filter=&availableDeliveryFilter=&filterType=&isPriceRange=false&priceRange=&minPrice=&maxPrice=&page=1&trcid=&traid=&filterSetByUser=true&channel=user&backgroundColor=&component=&rating=0&sorter=salePriceDesc&listSize=36",
+				"샤넬",
+				"가방"
+				);
+		crawlingproduct(pl,
+				"https://www.coupang.com/np/search?rocketAll=false&q=%EC%83%A4%EB%84%AC+%EC%A7%80%EA%B0%91&brand=&offerCondition=&filter=&availableDeliveryFilter=&filterType=&isPriceRange=false&priceRange=&minPrice=&maxPrice=&page=1&trcid=&traid=&filterSetByUser=true&channel=user&backgroundColor=&component=&rating=0&sorter=salePriceDesc&listSize=36",
+				"샤넬",
+				"지갑"
+				);
+		crawlingproduct(pl,
+				"https://www.coupang.com/np/search?rocketAll=false&q=%EC%83%A4%EB%84%AC+%EB%AA%A9%EA%B1%B8%EC%9D%B4&brand=&offerCondition=&filter=&availableDeliveryFilter=&filterType=&isPriceRange=false&priceRange=&minPrice=&maxPrice=&page=1&trcid=&traid=&filterSetByUser=true&channel=user&backgroundColor=&component=&rating=0&sorter=salePriceDesc&listSize=36",
+				"샤넬",
+				"목걸이"
+				);
+		
+		// 에르메스 파트
+				crawlingproduct(pl,
+						"https://www.coupang.com/np/search?rocketAll=false&q=%EC%97%90%EB%A5%B4%EB%A9%94%EC%8A%A4+%EC%8B%9C%EA%B3%84&brand=&offerCondition=&filter=&availableDeliveryFilter=&filterType=&isPriceRange=false&priceRange=&minPrice=&maxPrice=&page=1&trcid=&traid=&filterSetByUser=true&channel=user&backgroundColor=&component=&rating=0&sorter=salePriceDesc&listSize=36",
+						"에르메스",
+						"시계"
+						);
+				crawlingproduct(pl,
+						"https://www.coupang.com/np/search?rocketAll=false&q=%EC%97%90%EB%A5%B4%EB%A9%94%EC%8A%A4+%EA%B0%80%EB%B0%A9&brand=&offerCondition=&filter=&availableDeliveryFilter=&filterType=&isPriceRange=false&priceRange=&minPrice=&maxPrice=&page=1&trcid=&traid=&filterSetByUser=true&channel=user&backgroundColor=&component=&rating=0&sorter=salePriceDesc&listSize=36",
+						"에르메스",
+						"가방"
+						);
+				crawlingproduct(pl,
+						"https://www.coupang.com/np/search?rocketAll=false&q=%EC%97%90%EB%A5%B4%EB%A9%94%EC%8A%A4+%EC%A7%80%EA%B0%91&brand=&offerCondition=&filter=&availableDeliveryFilter=&filterType=&isPriceRange=false&priceRange=&minPrice=&maxPrice=&page=1&trcid=&traid=&filterSetByUser=true&channel=user&backgroundColor=&component=&rating=0&sorter=salePriceDesc&listSize=36",
+						"에르메스",
+						"지갑"
+						);
+				crawlingproduct(pl,
+						"https://www.coupang.com/np/search?rocketAll=false&q=%EC%97%90%EB%A5%B4%EB%A9%94%EC%8A%A4+%EB%AA%A9%EA%B1%B8%EC%9D%B4&brand=&offerCondition=&filter=&availableDeliveryFilter=&filterType=&isPriceRange=false&priceRange=&minPrice=&maxPrice=&page=1&trcid=&traid=&filterSetByUser=true&channel=user&backgroundColor=&component=&rating=0&sorter=salePriceDesc&listSize=36",
+						"에르메스",
+						"목걸이"
+						);
 				
+
+				// 디올 파트
+				crawlingproduct(pl,
+						"https://www.coupang.com/np/search?rocketAll=false&q=%EB%94%94%EC%98%AC+%EC%8B%9C%EA%B3%84&brand=&offerCondition=&filter=&availableDeliveryFilter=&filterType=&isPriceRange=false&priceRange=&minPrice=&maxPrice=&page=1&trcid=&traid=&filterSetByUser=true&channel=user&backgroundColor=&component=&rating=0&sorter=salePriceDesc&listSize=36",
+						"디올",
+						"시계"
+						);
+				crawlingproduct(pl,
+						"https://www.coupang.com/np/search?rocketAll=false&q=%EB%94%94%EC%98%AC+%EA%B0%80%EB%B0%A9&brand=&offerCondition=&filter=&availableDeliveryFilter=&filterType=&isPriceRange=false&priceRange=&minPrice=&maxPrice=&page=1&trcid=&traid=&filterSetByUser=true&channel=user&backgroundColor=&component=&rating=0&sorter=salePriceDesc&listSize=36",
+						"디올",
+						"가방"
+						);
+				crawlingproduct(pl,
+						"https://www.coupang.com/np/search?rocketAll=false&q=%EB%94%94%EC%98%AC+%EC%A7%80%EA%B0%91&brand=&offerCondition=&filter=&availableDeliveryFilter=&filterType=&isPriceRange=false&priceRange=&minPrice=&maxPrice=&page=1&trcid=&traid=&filterSetByUser=true&channel=user&backgroundColor=&component=&rating=0&sorter=salePriceDesc&listSize=36",
+						"디올",
+						"지갑"
+						);
+				crawlingproduct(pl,
+						"https://www.coupang.com/np/search?rocketAll=false&q=%EB%94%94%EC%98%AC+%EB%AA%A9%EA%B1%B8%EC%9D%B4&brand=&offerCondition=&filter=&availableDeliveryFilter=&filterType=&isPriceRange=false&priceRange=&minPrice=&maxPrice=&page=1&trcid=&traid=&filterSetByUser=true&channel=user&backgroundColor=&component=&rating=0&sorter=salePriceDesc&listSize=36",
+						"디올",
+						"목걸이"
+						);
+				
+		
+		
+		
+		
 	}
 			private static void crawlingproduct(ArrayList<productdto> pl,String cURL,String selectBrand,String selectClass) {
 				String url = cURL;
