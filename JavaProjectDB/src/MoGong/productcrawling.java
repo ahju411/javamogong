38cc81c7a7a28c78d0d158dfb53857426d01cdae
유은철 <axmin123@naver.com>
2021-06-01 00:45:31 +0900

    상품코드 수정

--- JavaProjectDB/src/MoGong/productcrawling.java
+++ JavaProjectDB/src/MoGong/productcrawling.java
@@ -19,7 +19,7 @@ public class productcrawling {
 				String itemclass = null;
 				String itemimage = null;
 		
-		// 1. 구찌시계 크롤링  사이트 : 쿠팡
+		// 크롤링 사이트 : 쿠팡
 					String url = "https://www.coupang.com/np/search?rocketAll=false&q=%EA%B5%AC%EC%B0%8C+%EC%8B%9C%EA%B3%84&brand=&offerCondition=&filter=&availableDeliveryFilter=&filterType=&isPriceRange=false&priceRange=&minPrice=&maxPrice=&page=1&trcid=&traid=&filterSetByUser=true&channel=user&backgroundColor=&component=&rating=0&sorter=salePriceDesc&listSize=36";
 					Document doc = null;
 					Elements tmp;
@@ -31,12 +31,13 @@ public class productcrawling {
 					}
 					
 					//
-					// 상위 7개정보 추출하여 DB에 넣기
+					// 
 					
 					Elements element = doc.select("ul[id=\"productList\"]");
 					Elements images = 
 				             element.select("img[class=\"search-product-wrap-img\"]");  
-				 
+				
+					// 1. 구찌 시계 ( 이름 , 가격 , 이미지 ) 크롤링 및 브랜드,종류 지정
 					for(int i = 0 ; i <5;i++) {
 						itemid =i+1;
 						tmp = element.select("div.name"); // 상품이름
@@ -57,5 +58,62 @@ public class productcrawling {
 						pl.add(new productdto(itemid,itemname,itemprice,itembrand,itemclass,itemimage));
 				
 					}
+					
+					
+					// 2. 구찌 가방
+				   url = "https://www.coupang.com/np/search?rocketAll=false&q=%EA%B5%AC%EC%B0%8C+%EA%B0%80%EB%B0%A9&brand=&offerCondition=&filter=&availableDeliveryFilter=&filterType=&isPriceRange=false&priceRange=&minPrice=&maxPrice=&page=1&trcid=&traid=&filterSetByUser=true&channel=user&backgroundColor=&component=&rating=0&sorter=salePriceDesc&listSize=36";
+				   doc = null;
+			
+				
+					try {
+						doc = Jsoup.connect(url).get();
+					}catch (Exception e) {
+						e.printStackTrace();
+					}
+					 element = doc.select("ul[id=\"productList\"]");
+				      images = 
+				             element.select("img[class=\"search-product-wrap-img\"]");  
+					
+					for(int i=0 ; i <5;i++) {
+						itemid =i+6;
+						tmp = element.select("div.name"); // 상품이름
+						itemname=(tmp.get(i).text());
+						
+						tmp = element.select("strong.price-value"); //상품가격
+						itemprice=(tmp.get(i).text());
+						
+						
+						itembrand="구찌";
+					
+						itemclass = "가방";
+						
+						itemimage  = images.get(i).attr("src");
+						
+					
+						pl.add(new productdto(itemid,itemname,itemprice,itembrand,itemclass,itemimage));
+				
+					}
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
 	}
 }
