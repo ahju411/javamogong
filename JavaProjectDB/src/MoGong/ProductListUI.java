cad9093a01b5547ddf6838b8e93ed17c5e92971c
xogus <xogus@LAPTOP-PV9T91JP>
2021-06-11 02:01:16 +0900

    그럼 20000

--- JavaProjectDB/src/MoGong/ProductListUI.java
+++ JavaProjectDB/src/MoGong/ProductListUI.java
@@ -44,46 +44,42 @@ import javax.swing.table.TableColumnModel;
 
 public class ProductListUI extends JFrame implements MouseListener, ActionListener {
 
-	private ImageIcon background,img1,buyimg;
+	private ImageIcon background, img1, buyimg;
 	private JLabel lblimg1;
 	private JLabel price1;
 	private JScrollPane scrollpane;
-	private JLabel buy1,buy2,buy3,buy4,buy5;
+	private JLabel buy1, buy2, buy3, buy4, buy5;
 	private JTable table;
 	private DefaultTableModel tModel;
 	private JButton btnback;
 
-
 	private JMenu menuProduct;
-	private JMenu  MENUBRAND;
-	private JMenuItem WATCH,BAG,WALLET,NECKLACE;
-	
+	private JMenu MENUBRAND;
+	private JMenuItem WATCH, BAG, WALLET, NECKLACE;
 
-	
 	private JMenu menuUser;
-	private JMenuItem BuyList,basket,MyInfo,Reservation,Logout;
+	private JMenuItem BuyList, basket, MyInfo, Reservation, Logout;
 	private JPanel pan;
 	private JLabel lbl;
 
 	private JMenuBar menubar;
-	
+
 	mainFrame mainFrame;
 	private int l;
 	String id;
 
-	public ProductListUI(String title , int width , int height,int n,mainFrame mainFrame,String id) {
+	public ProductListUI(String title, int width, int height, int n, mainFrame mainFrame, String id) {
 		setTitle(title);
-		setSize(width,height);
+		setSize(width, height);
 		setLocationRelativeTo(this);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.mainFrame = mainFrame;
 		this.id = id;
 
-		
-		// 메뉴 
+		// 메뉴
 		menubar = new JMenuBar();
 		menubar.setBackground(Color.PINK);
-	
+
 		menuUser = new JMenu("My");
 		MyInfo = new JMenuItem("나의 정보");
 		MyInfo.addActionListener(this);
@@ -92,104 +88,92 @@ public class ProductListUI extends JFrame implements MouseListener, ActionListen
 		BuyList = new JMenuItem("구매목록");
 		Logout = new JMenuItem("로그아웃");
 		Logout.addActionListener(this);
-	
+
 		menuUser.add(MyInfo);
 		menuUser.add(Reservation);
 		menuUser.add(basket);
 		menuUser.add(BuyList);
 		menuUser.add(Logout);
-	//
-		
-		
-		
-	
+		//
+
 		outputProductList(n);
-		
-		 l = n;
-		
+
+		l = n;
+
 		//
 		menubar.add(menuUser);
-		setJMenuBar(menubar);	
-		
+		setJMenuBar(menubar);
+
 		//
 		//
-		
+
 		//
 		//
-		
-		
+
 		setVisible(true);
 	}
-	
+
 // 누른  상품종류별로 그 항목이 뜨게하는 메소드
 	private void outputProductList(int n) {
 		pan = new JPanel(new BorderLayout());
-		pan.setBorder(new EmptyBorder(5,5,5,5));
+		pan.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(pan);
-		
+
 		lbl = new JLabel("상품 목록");
-		lbl.setFont(new Font("맑은 고딕",Font.BOLD,30));
+		lbl.setFont(new Font("맑은 고딕", Font.BOLD, 30));
 		lbl.setHorizontalAlignment(SwingConstants.CENTER);
-		lbl.setPreferredSize(new Dimension(400,80));
-		
-		pan.add(lbl,BorderLayout.NORTH);
-		
-        ProductDB db = new ProductDB();
+		lbl.setPreferredSize(new Dimension(400, 80));
+
+		pan.add(lbl, BorderLayout.NORTH);
+
+		ProductDB db = new ProductDB();
 		List<productdto> list = db.getProductList();
-		
-		String header[] = {"상품이름","상품할인가격","상품브랜드","상품종류","상품사진"};
-		
-		tModel = new DefaultTableModel(header,0) {
+
+		String header[] = { "상품이름", "상품할인가격", "상품브랜드", "상품종류", "상품사진" };
+
+		tModel = new DefaultTableModel(header, 0) {
 			public Class getColumnClass(int column) {
-				return getValueAt(0	, column).getClass();
+				return getValueAt(0, column).getClass();
 			}
+
 			public boolean isCellEditable(int i, int c) {
 				return false;
 			}
-			
+
 		};
 		// i는 5의배수로 넣을예정. 총 16개
-		for(int i = n+0; i<n+5; i++) {
+		for (int i = n + 0; i < n + 5; i++) {
 			int itemid = list.get(i).getItemid();
 			String itemname = list.get(i).getItemname();
 			String itemprice = list.get(i).getItemprice();
 			String itembrand = list.get(i).getItembrand();
 			String itemclass = list.get(i).getItemclass();
-			String itemimage =list.get(i).getItemimage();
-			
-			// 할인된가격 표시하기 
-			itemprice = itemprice.replace(",", "");  // 가격에 ' , ' 빼기
-			DecimalFormat dc = new DecimalFormat("###,###,###");  // 점 넣기
-			double SpotDel =(double)((Integer.parseInt(itemprice) * 0.9)); // 10%할인 적용
+			String itemimage = list.get(i).getItemimage();
+
+			// 할인된가격 표시하기
+			itemprice = itemprice.replace(",", ""); // 가격에 ' , ' 빼기
+			DecimalFormat dc = new DecimalFormat("###,###,###"); // 점 넣기
+			double SpotDel = (double) ((Integer.parseInt(itemprice) * 0.9)); // 10%할인 적용
 			String saleprice = dc.format(SpotDel); // 점 넣기
-			
+
 			Icon icon;
 			try {
-				URL url = new URL("https:"+itemimage);
+				URL url = new URL("https:" + itemimage);
 				icon = new ImageIcon(url);
-				Object [] data = {itemname,saleprice+" (10% SALE)",itembrand,itemclass,icon};
-				
+				Object[] data = { itemname, saleprice + " (10% SALE)", itembrand, itemclass, icon };
+
 				tModel.addRow(data);
 			} catch (MalformedURLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		
-		
-				
-				//근데 이렇게 하잖아?
-				
-			
-		
-	
-			
-			
+
+			// 근데 이렇게 하잖아?
+
 		}
-		
-		
 
 		table = new JTable(tModel);
-		
+
 		resizeColumnWidth(table);
 		table.setFont(new Font("맑온 고딕", Font.PLAIN, 15));
 		table.setRowHeight(200);
@@ -202,42 +186,38 @@ public class ProductListUI extends JFrame implements MouseListener, ActionListen
 		 */
 		table.setCellSelectionEnabled(false);
 		table.addMouseListener(this);
-		
+
 		JScrollPane scrollPane = new JScrollPane(table);
-		pan.add(scrollPane,BorderLayout.CENTER);
-		
+		pan.add(scrollPane, BorderLayout.CENTER);
+
 		btnback = new JButton("뒤로가기");
 		btnback.addActionListener(this);
-		pan.add(btnback,BorderLayout.SOUTH);
-		
-	}
+		pan.add(btnback, BorderLayout.SOUTH);
 
+	}
 
 	public static void main(String[] args) {
-		
-	}
-	
-	
-	public void resizeColumnWidth(JTable table) {
-		
-		
-		final TableColumnModel columnModel = table.getColumnModel(); 
-	for (int column = 0; column < table.getColumnCount(); column++) {
-	
-		int width = 30; // Min width for 
-		for(int row = 0; row < table.getRowCount(); row++) {
-		
-		TableCellRenderer renderer = table.getCellRenderer(row, column);
-		Component comp = table.prepareRenderer(renderer, row, column); 
-		width = Math.max(comp.getPreferredSize().width +1 , width); 
-		
-	}
-		
-		columnModel.getColumn(column).setPreferredWidth(width); 
-		
-	}
-}
 
+	}
+
+	public void resizeColumnWidth(JTable table) {
+
+		final TableColumnModel columnModel = table.getColumnModel();
+		for (int column = 0; column < table.getColumnCount(); column++) {
+
+			int width = 30; // Min width for
+			for (int row = 0; row < table.getRowCount(); row++) {
+
+				TableCellRenderer renderer = table.getCellRenderer(row, column);
+				Component comp = table.prepareRenderer(renderer, row, column);
+				width = Math.max(comp.getPreferredSize().width + 1, width);
+
+			}
+
+			columnModel.getColumn(column).setPreferredWidth(width);
+
+		}
+	}
 
 	ImageIcon imageSetSize(ImageIcon icon, int i, int j) {
 		Image ximg = icon.getImage();
@@ -249,60 +229,52 @@ public class ProductListUI extends JFrame implements MouseListener, ActionListen
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		Object obj = e.getSource();
-		
-		if(obj == price1) {
-			System.out.println("TEst");
-			
-		}
-		
-		if(e.getClickCount() == 2) {
+
+		if (e.getClickCount() == 2) {
 			int r = table.getSelectedRow();
 			int tmp;
-			tmp = l+r;
+			tmp = l + r;
 			try {
-				productInfo pby = new productInfo("상품정보화면/" + id, 650, 800, tmp,id);
+				productInfo pby = new productInfo("상품정보화면/" + id, 650, 800, tmp, id);
 			} catch (MalformedURLException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
 		}
-		
+
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-	 
+
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-	
+
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-	
+
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-	
-	}
 
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		Object obj = e.getSource();
-		if(obj == btnback) {
+		if (obj == btnback) {
 			mainFrame.setVisible(true);
 			dispose();
-			}
-	
-		
-		
+		}
+
 	}
-	
+
 }
--- JavaProjectDB/src/MoGong/productInfo.java
+++ JavaProjectDB/src/MoGong/productInfo.java
@@ -25,7 +25,6 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.SwingConstants;
 
-
 public class productInfo extends JFrame implements ActionListener {
 
 	private int res = 10, m;
@@ -35,57 +34,58 @@ public class productInfo extends JFrame implements ActionListener {
 	private String itemname, itemprice, itembrand, itemclass, itemimage;
 	private Object itemid;
 	String id;
-	
-	public productInfo(String title , int width , int height, int n, String id) throws MalformedURLException {
+
+	public productInfo(String title, int width, int height, int n, String id) throws MalformedURLException {
 		setTitle(title);
-		setSize(width,height);
+		setSize(width, height);
 		setLocationRelativeTo(this);
 		this.id = id;
-		//setLocation(250, 150);
-		
+		// setLocation(250, 150);
+
 		JPanel backpan = new JPanel();
 		backpan.setBackground(Color.WHITE);
 		JScrollPane scrollpane = new JScrollPane(backpan);
-		
+
 		backpan.setLayout(new BorderLayout());
-		
+
 		outputProductListSpec(n); // 선택한 상품 출력
-		m=n; // 다음 buyframe 에 보낼 인자값 저장
+		m = n; // 다음 buyframe 에 보낼 인자값 저장
 		// 첫번째 팬 (사진넣기)
 		JPanel pan1 = new JPanel();
-		URL ImageURL = new URL("http:"+itemimage);
-	    img = new ImageIcon(ImageURL);
-        lblimg = new JLabel(img);
-        
+		URL ImageURL = new URL("http:" + itemimage);
+		img = new ImageIcon(ImageURL);
+		lblimg = new JLabel(img);
+
 		pan1.setOpaque(false);
 		pan1.add(lblimg);
-		backpan.add(pan1,BorderLayout.NORTH);
-		
+		backpan.add(pan1, BorderLayout.NORTH);
+
 		// 두번째 팬 ( 가격정보 및 상품정보)
 		JPanel pan2 = new JPanel();
-		pan2.setLayout(new GridLayout(6,1));
+		pan2.setLayout(new GridLayout(6, 1));
 		DecimalFormat dc = new DecimalFormat("###,###,###"); // 가격에 반점 넣기
-	
-		JLabel name = new JLabel(itemname,SwingConstants.CENTER);
-		
+
+		JLabel name = new JLabel(itemname, SwingConstants.CENTER);
+
 		String ch = dc.format(Integer.parseInt(itemprice)); // 반점넣기
-		
-		JLabel jungga = new JLabel(ch+"원",SwingConstants.CENTER);
-		
-		double saleprice =(double)((Integer.parseInt(itemprice) * 0.9)); // 10%할인 적용
-	    ch = dc.format(saleprice);
-	    
-		JLabel price = new JLabel(ch+"원 (10% SALE)",SwingConstants.CENTER); // 10%할인한 가격 넣기
-		
-		//몇명 남았는지 표기 위한 DB 작성중
-		/*try {
+
+		JLabel jungga = new JLabel(ch + "원", SwingConstants.CENTER);
+
+		double saleprice = (double) ((Integer.parseInt(itemprice) * 0.9)); // 10%할인 적용
+		ch = dc.format(saleprice);
+
+		JLabel price = new JLabel(ch + "원 (10% SALE)", SwingConstants.CENTER); // 10%할인한 가격 넣기
+
+		// 몇명 남았는지 표기 위한 DB
+		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@118.217.168.174:1521:xe", "comet", "1234");
 			Statement stmt = conn.createStatement();
-			
-			ResultSet rs = stmt.executeQuery("SELECT count(itemid) FROM orders WHERE ITEMID = '" + itemid +  "' group by");
-			
-			if(rs.next()) {
+
+			ResultSet rs = stmt
+					.executeQuery("SELECT count(itemid) FROM orders WHERE itemid = '" + itemid + "' group by itemid");
+
+			if (rs.next()) {
 				res = 10 - rs.getInt(1);
 			}
 			conn.close();
@@ -96,97 +96,80 @@ public class productInfo extends JFrame implements ActionListener {
 			System.err.println("DB연결 오류 또는 쿼리 오류 입니다.");
 			e1.printStackTrace();
 		}
-		*/
-		//몇명 남았는지 표기
-		wait = new JLabel(" 남은 구매 예약자 : " + res + "명 ",SwingConstants.CENTER);
+
+		// 몇명 남았는지 표기
+		wait = new JLabel(" 남은 구매 예약자 : " + res + "명 ", SwingConstants.CENTER);
 		pan2.add(name);
 		pan2.add(jungga);
 		pan2.add(price);
 		pan2.add(wait);
-		
+
 		// 폰트설정
-		name.setFont(new Font("맑은 고딕",Font.BOLD,12));
-		jungga.setFont(new Font("맑은 고딕",Font.BOLD,15));
-		price.setFont(new Font("맑은 고딕",Font.BOLD,30));
+		name.setFont(new Font("맑은 고딕", Font.BOLD, 12));
+		jungga.setFont(new Font("맑은 고딕", Font.BOLD, 15));
+		price.setFont(new Font("맑은 고딕", Font.BOLD, 30));
 		price.setForeground(Color.RED);
 
-		wait.setFont(new Font("맑은 고딕",Font.BOLD,20));
+		wait.setFont(new Font("맑은 고딕", Font.BOLD, 20));
 		wait.setForeground(Color.RED);
-		
+
 		JPanel subpan2_1 = new JPanel();
-		
-		btnBuy= new JButton("구매예약");
+
+		btnBuy = new JButton("구매예약");
 		btnBuy.addActionListener(this);
-		
-		btnres= new JButton("예약취소");
+
+		btnres = new JButton("예약취소");
 		btnres.addActionListener(this);
 		btnres.setEnabled(false);
 
-		
 		subpan2_1.add(btnBuy);
-        subpan2_1.add(btnres);
+		subpan2_1.add(btnres);
 		subpan2_1.setOpaque(false);
 		pan2.add(subpan2_1);
-		
-		
+
 		pan2.setOpaque(false);
-		
-		
+
 		// 상품정보 pan3
 		JPanel pan3 = new JPanel();
 		pan3.setLayout(new GridLayout());
-		
+
 		infoimg = new ImageIcon("images/info2.png");
 		infoimg = imageSetSize(infoimg, 600, 2000);
 		lblinfoimg = new JLabel(infoimg);
-		
+
 		pan3.add(lblinfoimg);
 		pan3.setOpaque(false);
-		
 
-        
 		backpan.add(pan2);
-		backpan.add(pan3,BorderLayout.SOUTH);
-		
-		
-		
+		backpan.add(pan3, BorderLayout.SOUTH);
+
 		add(scrollpane);
-		
+
 		setVisible(true);
-		
-		
+
 	}
-	
-	public static void main(String[] args) throws MalformedURLException {
-	
-	
-	}
+
+	// public static void main(String[] args) throws MalformedURLException {
+
+	// }
 
 	private void outputProductListSpec(int n) {
-        ProductDB db = new ProductDB();
+		ProductDB db = new ProductDB();
 		List<productdto> list = db.getProductList();
 
 		// i는 5의배수로 넣을예정. 총 16개
-		
-			itemid = list.get(n).getItemid();
+
+		itemid = list.get(n).getItemid();
 		itemname = list.get(n).getItemname();
 		//
-		 itemprice = list.get(n).getItemprice();
-		 itembrand = list.get(n).getItembrand();
-		 itemclass = list.get(n).getItemclass();
-		 itemimage =list.get(n).getItemimage();
-		
-	      itemprice = itemprice.replace(",", "");  // ,을 제거하여 나중에 int로 변환하여 할인하기 편하게함.
+		itemprice = list.get(n).getItemprice();
+		itembrand = list.get(n).getItembrand();
+		itemclass = list.get(n).getItemclass();
+		itemimage = list.get(n).getItemimage();
 
-			}
-	
+		itemprice = itemprice.replace(",", ""); // ,을 제거하여 나중에 int로 변환하여 할인하기 편하게함.
 
-	
-		
-
-			
-		
-
+	}
 
 	ImageIcon imageSetSize(ImageIcon icon, int i, int j) {
 		Image ximg = icon.getImage();
@@ -197,20 +180,23 @@ public class productInfo extends JFrame implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
+
 		Object obj = e.getSource();
-		
-		if(obj == btnBuy) {
-			
-			//입력 DB 작성중
-			/*try {
+
+		if (obj == btnBuy) {
+
+			// 입력 DB 작성중
+
+			try {
 				Class.forName("oracle.jdbc.driver.OracleDriver");
-				Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@118.217.168.174:1521:xe", "comet", "1234");
+				Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@118.217.168.174:1521:xe", "comet",
+						"1234");
 				Statement stmt = conn.createStatement();
-				
-				ResultSet rs = stmt.executeQuery("SELECT count(itemid) FROM orders WHERE ITEMID = '" + itemid +  "' group by");
-				
-				if(rs.next()) {
+
+				ResultSet rs = stmt
+						.executeQuery("SELECT count(itemid) FROM orders WHERE ITEMID = '" + itemid + "' group by");
+
+				if (rs.next()) {
 					res = 10 - rs.getInt(1);
 				}
 				conn.close();
@@ -220,13 +206,13 @@ public class productInfo extends JFrame implements ActionListener {
 			} catch (SQLException e1) {
 				System.err.println("DB연결 오류 또는 쿼리 오류 입니다.");
 				e1.printStackTrace();
-			}*/
-			
+			}
+
 			btnres.setEnabled(true);
 			btnBuy.setEnabled(false);
-			
-			//res--;
-			
+
+			// res--;
+
 			wait.setText(" 남은 구매 예약자 : " + res + "명 ");
 			try {
 				BuyFrame bs = new BuyFrame("결제화면/" + id, 800, 800, m, id);
@@ -234,40 +220,33 @@ public class productInfo extends JFrame implements ActionListener {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
-		}else if(obj == btnres) {
+		} else if (obj == btnres) {
 			btnres.setEnabled(false);
 			btnBuy.setEnabled(true);
-			
-			
-			//삭제 DB 작성중
-			/*try {
-				Class.forName("oracle.jdbc.driver.OracleDriver");
-				Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@118.217.168.174:1521:xe", "comet", "1234");
-				Statement stmt = conn.createStatement();
-				
-				ResultSet rs = stmt.executeQuery("DELETE FROM ORDERS WHERE ID = '" + id + "' AND ITEMID = (SELECT itemid FROM item WHERE itemname = '" + itemname + "')");
-				
-				if(rs.next()) {
-					res = 10 - rs.getInt(1);
-				}
-				conn.close();
-			} catch (ClassNotFoundException e1) {
-				System.out.println("JDBC드라이버 로드 에러");
-				e1.printStackTrace();
-			} catch (SQLException e1) {
-				System.err.println("DB연결 오류 또는 쿼리 오류 입니다.");
-				e1.printStackTrace();
-			}
-			*/
-			//res++;
+
+			// 삭제 DB 작성중
+			/*
+			 * try { Class.forName("oracle.jdbc.driver.OracleDriver"); Connection conn =
+			 * DriverManager.getConnection("jdbc:oracle:thin:@118.217.168.174:1521:xe",
+			 * "comet", "1234"); Statement stmt = conn.createStatement();
+			 * 
+			 * ResultSet rs = stmt.executeQuery("DELETE FROM ORDERS WHERE ID = '" + id +
+			 * "' AND ITEMID = (SELECT itemid FROM item WHERE itemname = '" + itemname +
+			 * "')");
+			 * 
+			 * if(rs.next()) { res = 10 - rs.getInt(1); } conn.close(); } catch
+			 * (ClassNotFoundException e1) { System.out.println("JDBC드라이버 로드 에러");
+			 * e1.printStackTrace(); } catch (SQLException e1) {
+			 * System.err.println("DB연결 오류 또는 쿼리 오류 입니다."); e1.printStackTrace(); }
+			 */
+			// res++;
 			wait.setText(" 남은 구매 예약자 : " + res + "명 ");
 		}
-		
-	}
-	
-	public void DBProdu(){
-		
+
 	}
 
+	public void DBProdu() {
+
+	}
 
 }
